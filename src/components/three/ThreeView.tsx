354946146b265d243a94f6c80e30b2f9c7613052
Polarts <davidklein.4496@gmail.com>
2022-05-28 17:10:22 +0300

    Added initial 3D view implementation

--- src/components/three/ThreeView.tsx
+++ src/components/three/ThreeView.tsx
@@ -1,5 +1,41 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Vector3, Mesh } from "three";
+
+type BoxProps = {
+    position: Vector3
+}
+
+function Box(props: BoxProps) {
+    // This reference gives us direct access to the THREE.Mesh object
+    const ref = useRef<Mesh>(null)
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current!.rotation.x += 0.01))
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    )
+  }
+
 export default function ThreeView() {
     return (
-        <p>Here lies the 3D view of the application</p>
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={new Vector3(-1.2, 0, 0)} />
+            <Box position={new Vector3(1.2, 0, 0)} />
+        </Canvas>
     )
 }
