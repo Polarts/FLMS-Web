77fc9383425e6ce5ac6156300d0e8a9d4b70a41a
Polarts <davidklein.4496@gmail.com>
2022-05-28 22:23:34 +0300

    Added jDataView and attempting to mimic bini reader

--- src/components/ui/menus/FileMenu.tsx
+++ src/components/ui/menus/FileMenu.tsx
@@ -1,16 +1,36 @@
 import { Menu, MenuDivider, MenuItem, SubMenu } from "@szhsin/react-menu";
-import Icon from '@mdi/react';
 import { mdiFolderSearchOutline, mdiExitToApp, mdiFileExcelOutline, mdiContentSaveOutline, mdiContentSaveMoveOutline, mdiContentSaveAllOutline } from '@mdi/js';
 import { mdiFileDocumentPlusOutline } from '../../../communityIcons';
 import IconMenuItem from "./IconMenuItem";
+import { useContext } from "react";
+import { FileContext } from "../../../data/context/FileContext";
+import jDataView from "jdataview";
 
 const tempFileTypes = ["INI File"];
 const tempRecentFiles = ["sample.ini", "system.ini", "universe.ini"];
 
 export default function FileMenu() {
 
+    const { pickFile, saveFile } = useContext(FileContext);
+
     function browse() {
-        
+        pickFile(async (fileList) => {
+            if (!!fileList && fileList.length === 1) {
+                const fr = new FileReader();
+                fr.readAsArrayBuffer(fileList[0]);
+                fr.onload = (e) => {
+                    const buffer = e.target?.result as ArrayBuffer;
+                    const bytes = new Uint8Array(buffer);
+                    const jdv = new jDataView(bytes);
+                    const stringBlockOffset = jdv.getInt32();
+                    console.log(stringBlockOffset);
+                    jdv.skip(stringBlockOffset);
+                    const strBlock = jdv.getString(buffer.byteLength, stringBlockOffset);
+                    console.log(strBlock);
+                    
+                }
+            }
+        }, ".ini");
     }
 
     return (
