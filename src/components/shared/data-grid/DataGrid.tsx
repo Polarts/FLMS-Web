deb0b04a73c7a5aba4c49449f3694127589029fa
Polarts <davidklein.4496@gmail.com>
2023-07-15 15:50:56 +0300

    Added tbody to fix console error

--- src/components/shared/data-grid/DataGrid.tsx
+++ src/components/shared/data-grid/DataGrid.tsx
@@ -9,18 +9,20 @@ export default function DataGrid({ data, onValueChange }: DataGridProps) {
 
     return (
         <table className="data-grid">
-            {Object.entries(data).map(([key, value]) => (
-                <tr key={key}>
-                    <td className="key-cell">
-                        <div className="key-cell-content" title={key}>
-                            {key}
-                        </div>
-                    </td>
-                    <td className="value-cell">
-                        <input type="text" value={value} onChange={(e) => onValueChange(key, e.target.value)} />
-                    </td>
-                </tr>
-            ))}
+            <tbody>
+                {Object.entries(data).map(([key, value]) => (
+                    <tr key={key}>
+                        <td className="key-cell">
+                            <div className="key-cell-content" title={key}>
+                                {key}
+                            </div>
+                        </td>
+                        <td className="value-cell">
+                            <input type="text" value={value} onChange={(e) => onValueChange(key, e.target.value)} />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
         </table>
     )
 }
